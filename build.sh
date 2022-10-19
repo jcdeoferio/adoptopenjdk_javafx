1f20130f6db64556be43a23c928701187a19915f
Juan Carlo Deoferio <jc.deoferio@gmail.com>
2022-10-19 19:08:39 +0800

    Move to eclipse-temurin and push 8u345-b01

--- build.sh
+++ build.sh
@@ -1,4 +1,4 @@
 docker build -t jcdeoferio/adoptopenjdk_javafx .
-docker tag jcdeoferio/adoptopenjdk_javafx:latest jcdeoferio/adoptopenjdk_javafx:8u292-b10-jdk-hotspot
+docker tag jcdeoferio/adoptopenjdk_javafx:latest jcdeoferio/adoptopenjdk_javafx:8u345-b01-jdk
 docker push -a jcdeoferio/adoptopenjdk_javafx
 

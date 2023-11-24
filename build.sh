ca40da7f90b5e973bbd0bf9c8e8c9077268cd068
jcdeoferio <270303+jcdeoferio@users.noreply.github.com>
2023-11-24 17:14:49 +0800

    Add custom jdk21 docker image with curl and git

--- build.sh
+++ build.sh
@@ -2,3 +2,7 @@ docker build -t jcdeoferio/adoptopenjdk_javafx .
 docker tag jcdeoferio/adoptopenjdk_javafx:latest jcdeoferio/adoptopenjdk_javafx:8u392-b08-jdk
 docker push -a jcdeoferio/adoptopenjdk_javafx
 
+cd jdk_curl
+docker build -t jcdeoferio/eclipse-temurin-curl .
+docker tag jcdeoferio/eclipse-temurin-curl:latest jcdeoferio/eclipse-temurin-curl:21.0.1_12-jdk-alpine
+docker push -a jcdeoferio/eclipse-temurin-curl
